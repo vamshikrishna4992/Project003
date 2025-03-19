ad49402530df2b9f4416ea08af8f8443d5014788
Vamshikrishnaa <vamshikrishna4992@gmail.com>
2025-03-19 14:42:14 +0530

    Initial set up added

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-const express = require("express")
-const port = 3000
+import express from 'express';
+import dotenv from 'dotenv';
 
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
